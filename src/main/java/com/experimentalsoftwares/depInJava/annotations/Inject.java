a5776f3e41bc5700020ccbfa072fb59311928fdf
ensar <rasnesakam@gmail.com>
2021-06-10 17:09:00 +0300

    project created

---
+++ src/main/java/com/experimentalsoftwares/depInJava/annotations/Inject.java
@@ -0,0 +1,8 @@
+package com.experimentalsoftwares.depInJava.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
