d44ba9039bca29a20e4000f17488181edcd4b49d
ensar <rasnesakam@gmail.com>
2021-10-04 12:21:50 +0300

    JsonFieldMap implemented

--- src/main/java/com/experimentalsoftwares/depInJava/utils/referenceMappers/abstracts/FieldMap.java
+++ src/main/java/com/experimentalsoftwares/depInJava/utils/referenceMappers/abstracts/FieldMap.java
@@ -1,6 +1,5 @@
 package com.experimentalsoftwares.depInJava.utils.referenceMappers.abstracts;
 
-// TODO JsonFieldMap'i implement et
 // TODO XmlFieldMap'i implement et
 public interface FieldMap {
     String getFieldName();
--- src/main/java/com/experimentalsoftwares/depInJava/utils/referenceMappers/concretes/json/JsonFieldMap.java
+++ src/main/java/com/experimentalsoftwares/depInJava/utils/referenceMappers/concretes/json/JsonFieldMap.java
@@ -1,22 +1,34 @@
 package com.experimentalsoftwares.depInJava.utils.referenceMappers.concretes.json;
 
+import com.experimentalsoftwares.depInJava.utils.exceptions.JsonParserException;
 import com.experimentalsoftwares.depInJava.utils.referenceMappers.abstracts.ArgumentMap;
 import com.experimentalsoftwares.depInJava.utils.referenceMappers.abstracts.FieldMap;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonFieldMap implements FieldMap {
 
-    public JsonFieldMap(JSONObject jsonObject){
+    private final String name;
+    private final ArgumentMap argumentMap;
 
+    public JsonFieldMap(JSONObject jsonObject){
+        String FIELD_NAME = "fieldName", ARG = "arg";
+        try{
+            this.name = jsonObject.getString(FIELD_NAME);
+            this.argumentMap = new JsonArgumentMap(jsonObject.getJSONObject(ARG));
+        }
+        catch (JSONException exception){
+            throw JsonParserException.invalidJsonObject(exception);
+        }
     }
 
     @Override
     public String getFieldName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public ArgumentMap getArg() {
-        return null;
+        return this.argumentMap;
     }
 }
