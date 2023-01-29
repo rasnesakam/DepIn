43ffa1d49a8d17aba6f83a9460ecc401154c07df
ensar <ensar.makas@gmail.com>
2023-01-29 12:58:36 +0300

    Added new cases and classes for testing

---
+++ src/test/java/com/emakas/depIn/test/main/TestInstanceBuilder.java
@@ -0,0 +1,146 @@
+package com.emakas.depIn.test.main;
+
+import com.emakas.depIn.test.cases.case1.abstractClasses.ExampleAbstractClass;
+import com.emakas.depIn.test.cases.case1.classes.EmptyClass;
+import com.emakas.depIn.test.cases.case1.classes.ExampleClass;
+import com.emakas.depIn.test.cases.case1.enums.ExampleEnum;
+import com.emakas.depIn.test.cases.case1.interfaces.ExampleInterface;
+import com.emakas.depIn.test.cases.case1.recordClasses.ExampleRecord;
+import com.emakas.depIn.core.InstanceBuilder;
+import com.emakas.depIn.utils.exceptions.ClassBuildException;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestInstanceBuilder {
+
+    @Test
+    public void testDefaultClass() {
+        Exception exception = assertThrows(ClassBuildException.class,()->{
+            DefaultClass defaultClass = InstanceBuilder.init(DefaultClass.class).build();
+        });
+
+        assertTrue(
+                exception.getCause() instanceof IllegalAccessException
+                ,String.format(
+                        "Thrown exception: %s"
+                        ,exception.getCause().getClass().getName()
+                )
+        );
+    }
+
+    @Test
+    public void testAbstractClass() {
+        Exception exception = assertThrows(ClassBuildException.class,()->{
+            ExampleAbstractClass abstractClass = InstanceBuilder.init(ExampleAbstractClass.class).build();
+        });
+
+        assertTrue(
+                exception.getCause() instanceof InstantiationException
+                ,String.format(
+                        "Thrown exception: %s"
+                        ,exception.getCause().getClass().getName()
+                )
+        );
+    }
+
+    @Test
+    public void testInterface() {
+        Exception exception = assertThrows(ClassBuildException.class,()->{
+            ExampleInterface exampleInterface = InstanceBuilder.init(ExampleInterface.class).build();
+        });
+
+        assertTrue(
+                exception.getCause() instanceof NoSuchMethodException //Interfaces has no constructor
+                ,String.format(
+                        "Thrown exception: %s"
+                        ,exception.getCause().getClass().getName()
+                )
+        );
+    }
+
+    @Test
+    public void testEnums() {
+        Exception exception = assertThrows(ClassBuildException.class,()->{
+            ExampleEnum exampleEnum = InstanceBuilder.init(ExampleEnum.class).build();
+        });
+
+        assertTrue(
+                exception.getCause() instanceof NoSuchMethodException // Enum has no public constructor
+                ,String.format(
+                        "Thrown exception: %s"
+                        ,exception.getCause().getClass().getName()
+                )
+        );
+    }
+
+    @Test
+    public void testRecords() {
+        ExampleRecord record = InstanceBuilder.init(ExampleRecord.class).withArgs("Selam").build();
+
+        assertNotNull(record,"Created instance is null");
+        assertNotNull(record.arg(),"Arg of created instance is null");
+    }
+
+    @Test
+    public void testEmptyClass() {
+        EmptyClass emptyClass = InstanceBuilder.init(EmptyClass.class).build();
+
+        assertNotNull(emptyClass,"Created instance is null");
+    }
+
+    @Test
+    public void testConstructor(){
+
+        ExampleClass ec = InstanceBuilder.init(ExampleClass.class).withArgs("Selam").build();
+        assertNotNull(ec,"Object is null");
+        assertNotNull(ec.getDependency());
+        assertEquals("Selam", ec.getDependency(),"Example class dependency is wrong!");
+
+    }
+
+    @Test
+    public void testPrivateFields(){
+        Map<String,Object> fieldsMap = new HashMap<>();
+        fieldsMap.put("dependency","Selam");
+        fieldsMap.put("secretString","gizli");
+
+        ExampleClass ec = InstanceBuilder.init(ExampleClass.class).withArgs("Aleykümselam").withFields(fieldsMap).build();
+
+        assertNotNull(ec,"Object is null");
+        assertNotNull(ec.getDependency());
+        assertNotNull(ec.getSecretString());
+        assertEquals("Selam", ec.getDependency(),"Example class dependency is wrong!");
+        assertEquals("gizli", ec.getSecretString(),"Example class dependency is wrong!");
+    }
+
+    @Test
+    public void testPublicFields(){
+
+        Map<String,Object> fieldsMap = new HashMap<>();
+        fieldsMap.put("publicDependency","Selam");
+
+        ExampleClass ec = InstanceBuilder.init(ExampleClass.class).withFields(fieldsMap).build();
+
+        assertNotNull(ec,"Object is null");
+        assertNotNull(ec.publicDependency);
+        assertEquals("Selam", ec.publicDependency,"Example class dependency is wrong!");
+
+    }
+    @Test
+    public void testSetterInjection(){
+
+        Map<String,Object> setterMap = new HashMap<>();
+        setterMap.put("setSecretString","Selam");
+        ExampleClass ec = InstanceBuilder.init(ExampleClass.class).withSetters(setterMap).build();
+
+        assertNotNull(ec,"Object is null");
+        assertNotNull(ec.getSecretString());
+        assertEquals("Selam", ec.getSecretString(),"Example class dependency is wrong!");
+
+    }
+
+}
